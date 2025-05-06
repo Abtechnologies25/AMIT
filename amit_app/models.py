2f406e904562b746119f4d6b898a6709c42b7a99
Abttechnologies25 <abtechchennai@gmail.com>
2025-05-06 17:53:57 +0530

    ll

--- amit_app/models.py
+++ amit_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_delete
-from django.dispatch import receiver
-import os
+
 class GalleryCategory(models.Model):
     name = models.CharField(max_length=255)
 
@@ -11,13 +9,8 @@ class GalleryCategory(models.Model):
 class GalleryImage(models.Model):
     category = models.ForeignKey(GalleryCategory, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='gallery_images/')
-    name = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255, blank=True)  
 
     def __str__(self):
-        return self.name if self.name else self.image.name.split("/")[-1]
+        return self.name if self.name else "Unnamed Image"
 
-@receiver(models.signals.post_delete, sender=GalleryImage)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
