9d40402b0225defdea912b0638eb7f2ec8ce17a4
Abttechnologies25 <abtechchennai@gmail.com>
2025-05-23 12:08:24 +0530

    la

---
+++ amit_app/sitemaps.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+from django.shortcuts import reverse
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
+
+    def items(self):
+        return ['home_page', 'business_page', 'projects_page','training_page','contact_page','gallery_view','send_mail_page']  # Update with your actual view names
+
+    def location(self, item):
+        return reverse(item)
